5850b2383458d966a3efab434245a0dc16d9015f
Paulo Sergio <pauloinfinity8@gmail.com>
2023-05-12 13:29:28 -0300

    Get lists

--- src/main/java/com/devsuperior/dslist/service/GameService.java
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -17,10 +17,12 @@ import java.util.List;
 public class GameService {
     @Autowired
     private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public GameDTO findById(Long id){
         Game result = gameRepository.findById(id).get();
         return new GameDTO(result);
+
     }
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
